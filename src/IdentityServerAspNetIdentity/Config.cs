8dac2c0dbb0e27e3b330f5ad8619c5bf5d6ed4b1
Nemanja <nemanjarogic93@gmail.com>
2020-10-16 10:05:15 +0200

    Change config for asp net core identity

--- src/IdentityServerAspNetIdentity/Config.cs
+++ src/IdentityServerAspNetIdentity/Config.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
@@ -10,49 +11,83 @@ namespace IdentityServerAspNetIdentity
     public static class Config
     {
         public static IEnumerable<IdentityResource> IdentityResources =>
-                   new IdentityResource[]
-                   {
+            new List<IdentityResource>
+            {
                 new IdentityResources.OpenId(),
                 new IdentityResources.Profile(),
-                   };
+                new IdentityResource("favoriteProgrammingLanguage", new List<string> { "favoriteProgrammingLanguage" }),
+            };
 
         public static IEnumerable<ApiScope> ApiScopes =>
-            new ApiScope[]
+            new List<ApiScope>
             {
-                new ApiScope("scope1"),
-                new ApiScope("scope2"),
+                new ApiScope("api1", "My API")
             };
 
         public static IEnumerable<Client> Clients =>
-            new Client[]
+            new List<Client>
             {
-                // m2m client credentials flow client
+                // machine to machine client (console client from quickstart 1)
                 new Client
                 {
-                    ClientId = "m2m.client",
-                    ClientName = "Client Credentials Client",
+                    ClientId = "client",
 
+                    // no interactive user, use the clientid/secret for authentication
                     AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
 
-                    AllowedScopes = { "scope1" }
+                    // secret for authentication
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+
+                    // scopes that client has access to
+                    AllowedScopes = { "api1" }
                 },
 
-                // interactive client using code flow + pkce
+                // interactive ASP.NET Core MVC client
+                // OpenID Connect-based clients are very similar to the OAuth 2.0 clients above.
+                // But since the flows in OIDC are always interactive, we need to add some redirect URLs to our configuration.
                 new Client
                 {
-                    ClientId = "interactive",
-                    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
+                    ClientId = "mvc",
+                    ClientSecrets = { new Secret("secret".Sha256()) },
 
                     AllowedGrantTypes = GrantTypes.Code,
 
-                    RedirectUris = { "https://localhost:44300/signin-oidc" },
-                    FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-                    PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
+                    // where to redirect to after login
+                    RedirectUris = { "https://localhost:5002/signin-oidc" },
 
+                    // where to redirect to after logout
+                    PostLogoutRedirectUris = { "https://localhost:5002/signout-callback-oidc" },
+
+                    // Enable support for refresh tokens
                     AllowOfflineAccess = true,
-                    AllowedScopes = { "openid", "profile", "scope2" }
+
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "favoriteProgrammingLanguage",
+                        "api1"
+                    }
                 },
+                // JavaScript Client
+                new Client
+                {
+                    ClientId = "js",
+                    ClientName = "JavaScript Client",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequireClientSecret = false,
+
+                    RedirectUris =           { "https://localhost:5003/callback.html" },
+                    PostLogoutRedirectUris = { "https://localhost:5003/index.html" },
+                    AllowedCorsOrigins =     { "https://localhost:5003" },
+
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "api1"
+                    }
+                }
             };
     }
 }
